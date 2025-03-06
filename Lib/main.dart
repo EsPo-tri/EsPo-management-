adfb69f2d97165a5966c189a36e7f884267c722f
EsPo-tri <espo2435@gmail.com>
2025-03-06 20:08:09 +0530

    main.dart

--- Lib/main.dart
+++ Lib/main.dart
@@ -1,3 +1,31 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  print("Hello, EsPo!");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'EsPo App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('EsPo App')),
+      body: const Center(child: Text('Welcome to EsPo!')),
+    );
+  }
 }
